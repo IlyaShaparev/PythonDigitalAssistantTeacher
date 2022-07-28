a728d87fd5f38a5a1ed617054185df021e8a6b71
Ilya Bogaichuk <bis12012003@yandex.ru>
2022-07-28 21:45:28 +0300

    add task_1

---
+++ task_1.py
@@ -0,0 +1,15 @@
+from math import floor
+
+
+def main(z, x):
+    summ = x ** 4
+    summ -= 20 * (z / 67 + 16 * x ** 3) ** 5
+    summ += 64 * (floor(59 * x ** 3 - 53 * z ** 2 - 1)) ** 7
+    return summ
+
+
+print(main(-0.52, -0.46))           # = -1.60e+11
+print(main(0.79, 0.83))             # = -1.29e+06
+print(main(-0.31, -0.67))           # = -2.94e+11
+print(main(-0.95, 0.07))            # = -4.34e+13
+print(main(-0.35, 0.61))            # = 4.99e+06
