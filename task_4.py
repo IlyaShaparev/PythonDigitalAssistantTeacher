3983283479fc2699966ca9d29f6bf0d42e1e079c
Ilya Bogaichuk <bis12012003@yandex.ru>
2022-08-03 23:45:34 +0300

    correction task_4 and add img folder

--- task_4.py
+++ task_4.py
@@ -1,17 +1,17 @@
-from math import log
+from math import log2, atan
 
 
 def main(n):
     if n == 0:
-        return -0.02
+        return 0.93
     elif n == 1:
-        return -0.6
+        return 0.29
     else:
-        return 96 * (log((main(n - 1)) ** 2 + (main(n - 2)) ** 3)) ** 2 + 1
+        return (atan(43 * (main(n-1)) ** 3 - 1 - 15 * (main(n-2)) ** 2))/77 + log2(main(n-2)) ** 2 + 1
 
 
-print(main(2))          # = 1.01e+02
-print(main(9))          # = 1.16e+05
-print(main(8))          # = 1.13e+05
-print(main(3))          # = 8.19e+03
-print(main(4))          # = 3.12e+04
+print(main(6))          # = 1.02e+00
+print(main(4))          # = 1.02e+00
+print(main(7))          # = 6.74e+00
+print(main(9))          # = 8.56e+00
+print(main(3))          # = 4.21e+00
